5ca0317e60c3c234ff195cb069979e90f20d58d7
Gabriel Demes <gabrieldemes@nyu.edu>
2020-12-09 17:17:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   name_array = []
-  
+
   #collect all names
   data[color].each do |x,y|
     name_array += y
@@ -10,12 +10,12 @@ def nyc_pigeon_organizer(data)
   name_array = name_array.uniq
 
 """  #change each name to a symbol
-  name_array.map do |i| 
+  name_array.map do |i|
     i.to_sym
   end
 """
   #add name to keys
-  name_array.each do |i|  
+  name_array.each do |i|
     new_hash[i] = {color:[],
       gender:[]
       lives:[]
@@ -26,7 +26,7 @@ def nyc_pigeon_organizer(data)
     data[:color].each do |t,u|
       if u.include? x
         new_hash[x][:color] += t.to_s
-  
-  
-    
+
+
+
 end
